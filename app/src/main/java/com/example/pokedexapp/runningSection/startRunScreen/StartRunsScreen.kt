cf4140415fefc8a7b30ce6f9772cdc2c1c460c08
Filipedias0 <filipeandrade41@outlook.com>
2022-01-04 17:30:31 -0300

    Running section layouts WIP

--- app/src/main/java/com/example/pokedexapp/runningSection/startRunScreen/StartRunsScreen.kt
+++ app/src/main/java/com/example/pokedexapp/runningSection/startRunScreen/StartRunsScreen.kt
@@ -2,39 +2,33 @@ package com.example.pokedexapp.runningSection.startRunScreen
 
 import android.Manifest
 import android.os.Build
-import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.End
-import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.pokedexapp.R
 import com.example.pokedexapp.favPokemons.FavPokemonsViewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionsRequired
@@ -43,7 +37,7 @@ import com.google.accompanist.permissions.rememberMultiplePermissionsState
 @RequiresApi(Build.VERSION_CODES.Q)
 @ExperimentalPermissionsApi
 @Composable
-fun StartRunsScreen(
+fun StartRunScreen(
     navController: NavController,
     viewModel: FavPokemonsViewModel = hiltViewModel()
 ) {
@@ -67,52 +61,48 @@ fun StartRunsScreen(
                 .clip(RoundedCornerShape(10.dp))
                 .background(MaterialTheme.colors.secondary)
                 .padding(16.dp),
+            navController = navController
         )
-
     }
+
 }
 
 @Composable
 fun RunningWrapper(
     modifier: Modifier = Modifier,
+    navController: NavController
 ) {
-    var sortByState = remember { mutableStateOf("Date") }
-    val options = listOf("Date", "Distance")
+    var textWeight by rememberSaveable { mutableStateOf("Text") }
+    var textName by rememberSaveable { mutableStateOf("Text") }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
-        modifier = modifier,
+        modifier = modifier
     ) {
-        DropDown(
-            text = "Sort By: ${sortByState.value}",
-            options = options,
-            sortByState = sortByState,
-            modifier = Modifier
-                .padding(15.dp)
+
+        Image(
+            painter = painterResource(R.drawable.poke_ball_pin),
+            contentDescription = null,
+            modifier = Modifier.requiredSize(60.dp)
         )
 
-        Box(
-            contentAlignment = Alignment.BottomCenter,
+        Button(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(bottom = 22.dp)
-        ){
-            Row(
-                horizontalArrangement = Arrangement.End,
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                FloatingActionButton(
-                    elevation = FloatingActionButtonDefaults.elevation(12.dp, 12.dp),
-                    onClick = { },
-                    backgroundColor = Color(255, 203, 8),
-                    contentColor = Color.DarkGray,
-                    modifier = Modifier
-                ) {
-                    Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "Run")
-                }
-            }
+                .fillMaxWidth(0.5f),
+            onClick = {
+                navController.navigate(
+                    "runs_screen"
+                )
+            },
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(Color(255,203,8))){
+            Text(
+                text = "Run!",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color(0,103,180)
+            )
         }
     }
 }
@@ -198,10 +188,6 @@ fun PermissionsDialog(
 @Composable
 fun handlePermissions(showAlert: MutableState<Boolean>, background: Boolean) {
 
-    fun onCLickDialog() {
-        showAlert.value = false
-    }
-
     val permissions =
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q || !background) {
             listOf(
@@ -264,7 +250,7 @@ fun handlePermissions(showAlert: MutableState<Boolean>, background: Boolean) {
         }
 
     ) {
-        if (showAlert.value) {
+        if(showAlert.value){
             PermissionsDialog(
                 state = showAlert,
                 onClick = {
@@ -281,7 +267,7 @@ fun handlePermissions(showAlert: MutableState<Boolean>, background: Boolean) {
         }
     }
 
-    if (!permissionsBackState.allPermissionsGranted && permissionsState.allPermissionsGranted) {
+    if(!permissionsBackState.allPermissionsGranted && permissionsState.allPermissionsGranted){
         showAlert.value = true
 
         PermissionsDialog(
@@ -299,98 +285,4 @@ fun handlePermissions(showAlert: MutableState<Boolean>, background: Boolean) {
         }
     }
 }
-
-@Composable
-fun DropDown(
-    text: String,
-    modifier: Modifier = Modifier,
-    initiallyOpened: Boolean = false,
-    options: List<String>,
-    sortByState: MutableState<String>
-) {
-    var isOpen by remember {
-        mutableStateOf(initiallyOpened)
-    }
-
-    val alpha = animateFloatAsState(
-        targetValue = if (isOpen) 1f else 0f,
-        animationSpec = tween(
-            durationMillis = 300
-        )
-    )
-
-    val rotateX = animateFloatAsState(
-        targetValue = if (isOpen) 0f else -90f,
-        animationSpec = tween(
-            durationMillis = 300
-        )
-    )
-
-    Column(
-        horizontalAlignment = Start,
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable {
-                isOpen = !isOpen
-            }
-            .shadow(10.dp, RoundedCornerShape(10.dp))
-            .background(Color(255, 203, 8))
-            .clip(CircleShape)
-            .padding(12.dp, 8.dp, 12.dp, 0.dp)
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = text,
-                color = Color.DarkGray,
-                fontSize = 20.sp,
-            )
-            Icon(
-                imageVector = Icons.Default.ArrowDropDown,
-                contentDescription = "Open or close the dropdown",
-                tint = Color.DarkGray,
-                modifier = Modifier
-                    .scale(1f, if (isOpen) -1f else 1f)
-            )
-
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-    }
-
-
-    Column(
-        horizontalAlignment = Start,
-        modifier = Modifier
-            .graphicsLayer {
-                transformOrigin = TransformOrigin(0.5f, 0f)
-                rotationX = rotateX.value
-            }
-            .alpha(alpha.value)
-            .fillMaxWidth()
-            .shadow(10.dp, RoundedCornerShape(10.dp))
-            .background(Color(255, 203, 8))
-            .clip(CircleShape)
-            .padding(12.dp, 8.dp, 12.dp, 12.dp)
-    ) {
-        options.forEach {
-            Text(
-                text = it,
-                color = Color.DarkGray,
-                fontSize = 18.sp,
-                textAlign = TextAlign.Start,
-                modifier = Modifier
-                    .clickable {
-                        sortByState.value = it
-                        isOpen = false
-                    }
-            )
-        }
-    }
-}
-
-
 
