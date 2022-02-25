5052862252c9e5d78a8217897c42ac32c8302d1b
Filipedias0 <50221162+Filipedias0@users.noreply.github.com>
2022-02-25 11:25:49 -0300

    Launch screen

--- app/src/main/java/com/example/pokedexapp/MainActivity.kt
+++ app/src/main/java/com/example/pokedexapp/MainActivity.kt
@@ -1,8 +1,20 @@
 package com.example.pokedexapp
 
+import android.media.Image
+import android.os.Build
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -10,7 +22,9 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -22,6 +36,8 @@ import com.example.pokedexapp.runningSection.runsScreen.RunsScreen
 import com.example.pokedexapp.ui.theme.PokedexAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.*
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -30,6 +46,7 @@ import com.example.pokedexapp.runningSection.startRunScreen.StartRunScreen
 import com.example.pokedexapp.runningSection.statisticsScreen.StatisticsScreen
 import com.example.pokedexapp.runningSection.welcome.WelcomeScreen
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import kotlinx.coroutines.delay
 import java.util.*
 import javax.inject.Inject
 
@@ -39,8 +56,10 @@ class MainActivity : ComponentActivity() {
     @set:Inject
     var isFirstAppOpen = true
 
-    @ExperimentalPermissionsApi
-    @ExperimentalMaterialApi
+    @OptIn(ExperimentalPermissionsApi::class,
+        ExperimentalMaterialApi::class
+    )
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -83,16 +102,22 @@ class MainActivity : ComponentActivity() {
 
 
 
+@RequiresApi(Build.VERSION_CODES.Q)
 @ExperimentalPermissionsApi
 @Composable
 fun Navigation(navController: NavHostController, isFirstAppOpen: Boolean){
         NavHost(
             navController = navController,
-            startDestination = "pokemon_list_screen"
+            startDestination = "splash_screen"
         ) {
+            composable("splash_screen"){
+                SplashScreen(navController = navController)
+            }
+
             composable("pokemon_list_screen") {
                 PokemonListScreen(navController = navController)
             }
+
             composable(
                 "pokemon_detail_screen/{dominantColor}/{pokemonName}",
                 arguments = listOf(
@@ -113,7 +138,7 @@ fun Navigation(navController: NavHostController, isFirstAppOpen: Boolean){
                 }
                 PokemonDetailScreen(
                     dominantColor = dominantColor,
-                    pokemonName = pokemonName?.toLowerCase(Locale.ROOT) ?: "",
+                    pokemonName = pokemonName?.lowercase(Locale.ROOT) ?: "",
                     navController = navController
                 )
             }
@@ -178,5 +203,39 @@ fun BottomNavigationBar(
             )
         }
     }
+}
+@Composable
+fun SplashScreen(
+    navController: NavController
+){
+    val scale = remember{
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate("pokemon_list_screen")
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.secondary)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.poke_ball_pin),
+            contentDescription = "PokéBall pin (app icon)",
+            modifier = Modifier
+                .scale(scale.value)
+        )
 
+    }
 }
